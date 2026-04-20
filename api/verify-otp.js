37544bf2eacd472014e43e4d39892767b8ac1046
elizarenkovs-eng <elizarenkovs@gmail.com>
2026-04-21 02:40:58 +0300

    Update verify-otp.js

--- api/verify-otp.js
+++ api/verify-otp.js
@@ -1,5 +1,4 @@
-const { findContactByPhone, getClientContract, getManager } = require('./_bitrix');
-const { parseAddress } = require('./_bitrix');
+const { findContactByPhone, getClientContract, getManager, parseAddress } = require('./_bitrix');
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
